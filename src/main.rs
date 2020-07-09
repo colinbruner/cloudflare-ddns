a9619b6ee7a8e11ae48168d8c16cb58bcba3ab7b
Colin Bruner <colin.d.bruner@gmail.com>
2020-07-08 23:11:00 -0400

    use structs instead of generic values from serde

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // External libs
 use ipify_async;
 use reqwest::Client;
+use serde::Deserialize;
 use serde_json::{json, Value};
 
 // Local libs
@@ -9,37 +10,55 @@ use settings::Settings;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
 
-async fn get_zone_dns_records(client: &Client, settings: &Settings) -> Result<(String, String)> {
+#[derive(Deserialize)]
+struct DNSRecord {
+    #[serde(alias = "result")]
+    records: Vec<Records>,
+}
+
+#[derive(Deserialize)]
+struct Records {
+    #[serde(alias = "content")]
+    addr: String,
+    id: String,
+}
+
+async fn get_zone_dns_records(client: &Client, settings: &Settings) -> Result<DNSRecord> {
     // Returns the "content" of the A record
     let url = format!(
         "https://api.cloudflare.com/client/v4/zones/{}/dns_records",
         settings.zone_id
     );
     let auth = format!("Bearer {}", settings.token);
+
     let resp = client
         .get(&url)
         .header("Authorization", auth)
         .send()
         .await?;
-    let v: Value = serde_json::from_str(&resp.text().await?)?;
-    let addr: &str = v["result"][0]["content"].as_str().unwrap();
-    let id: &str = v["result"][0]["id"].as_str().unwrap();
-    Ok((String::from(addr), String::from(id)))
+
+    // Get DNS Record information
+    let record: DNSRecord = serde_json::from_str(&resp.text().await?)?;
+
+    // Get 'addr' and 'id' from record
+    //let addr: &str = record.result[0].content.as_str();
+    //let id: &str = record.result[0].id.as_str();
+
+    // Return results
+    Ok(record)
 }
 
 async fn update_zone_a_record_ip(
     client: &Client,
-    id: &str,
-    current_ip: &str,
+    dns_record: &DNSRecord,
     settings: &Settings,
 ) -> Result<String> {
     let url = format!(
         "https://api.cloudflare.com/client/v4/zones/{}/dns_records/{}",
-        settings.zone_id, id
+        settings.zone_id, dns_record.records[0].id
     );
     let auth = format!("Bearer {}", settings.token);
-    let payload =
-        json!({"type": "A", "name":settings.zone, "content": current_ip, "ttl":1, "proxied":true});
+    let payload = json!({"type": "A", "name":settings.zone, "content": dns_record.records[0].addr, "ttl":1, "proxied":true});
 
     let resp = client
         .put(&url)
@@ -58,16 +77,19 @@ async fn main() -> Result<()> {
     let settings = Settings::new().unwrap();
 
     let current_ip = ipify_async::get_ip().await.unwrap().to_string();
-    let (zone_ip, id) = get_zone_dns_records(&client, &settings).await?;
+    let dns_record: DNSRecord = get_zone_dns_records(&client, &settings).await?;
 
-    if current_ip != zone_ip {
-        println!("IP: {} == A Record {}. Doing nothing.", current_ip, zone_ip);
+    if current_ip == dns_record.records[0].addr {
+        println!(
+            "IP: {} == A Record {}. Doing nothing.",
+            current_ip, dns_record.records[0].addr
+        );
     } else {
         println!(
             "Updating A Record for Zone: {} to IP: {}.",
-            settings.zone, current_ip
+            settings.zone, dns_record.records[0].addr
         );
-        let status = update_zone_a_record_ip(&client, &id, &current_ip, &settings).await?;
+        let status = update_zone_a_record_ip(&client, &dns_record, &settings).await?;
         println! {"Success: {:?}", status};
     }
     Ok(())
